2901e2a3682104b00343e1d509a35ec6bc362a39
Elvis Mensah <elvismensah498@gmail.com>
2025-10-03 18:36:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+
+/*  -----    MAIN PAGE TEMPLATE      ------*/
+
+
+const body = document.querySelector('body') ;
+const buttonSwitch = document.querySelector('#mode-switch'); 
+
+
+
+/*  -----   CODE FOR DAY/NIGHT MODE    -----   */
+
+buttonSwitch.addEventListener('click', () => {
+ 
+ modeSwitch() ;
+})
+
+
+const modeSwitch = () => {
+  if(buttonSwitch.classList == 'day' ) {
+    body.style.backgroundColor = 'white' ;
+    body.style.color = 'black' ;
+    buttonSwitch.classList.remove('day') ;
+    buttonSwitch.classList.add('night') ;
+    buttonSwitch.style.animation = 'go 1s 1s 2' ;
+  }
+  else if(buttonSwitch.classList == 'night' ) {
+    body.style.backgroundColor = 'black' ;
+    body.style.color = 'white' ;
+    buttonSwitch.classList.remove('night') ;
+    buttonSwitch.classList.add('day') ;
+    buttonSwitch.style.animation = 'go 1s 1s 2' ;
+  }
+ buttonSwitch.style.animation = `go 1s ease-out 1s ` ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function action () {
+
+    try {
+        
+        const response = await fetch( 'https://api.adviceslip.com/advice') 
+
+        if(!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const adviceContent = data.slip.advice
+        const adviceSpace = document.querySelector('h2')
+        adviceSpace.innerHTML = adviceContent
+
+    }
+
+    catch (error) {
+        console.error(error) ;
+    }
+
+
+
+}
